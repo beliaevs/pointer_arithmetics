c00620c62e0a0be776760a332e6f3f6e4b808441
Alexander Beliaev <beliaevs@gmail.com>
2022-09-16 16:34:56 +0300

    added clang, some more pointer arithmetics

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ pointers.exe: pointers.cpp
 clean:
 	rm pointers.exe
 	
+clang: pointers_clang.exe
+
+pointers_clang.exe: pointers.cpp
+	clang++ -o pointers_clang.exe -std=c++17 -Wall pointers.cpp
--- pointers.cpp
+++ pointers.cpp
@@ -20,10 +20,31 @@ int main()
     {
         void* p = &a[0];
         // std::cout << *p << "\n"; //error: ‘void*’ is not a pointer-to-object type
-        ++p; // warning: ISO C++ forbids incrementing a pointer of type ‘void*’ [-Wpointer-arith]
+        // clang error: arithmetic on a pointer to void
+        // ++p; // g++ warning: ISO C++ forbids incrementing a pointer of type ‘void*’ [-Wpointer-arith]
         auto intp = static_cast<int*>(p);
-        std::cout << *intp << "\n"; // garbage
+        std::cout << *intp << "\n"; // 1
+        //void* p5 = &a[5];
+        //int count = 0;
+        //while(p != p5)
+        //{
+        //    ++p; // shift by 1 byte in gcc, error in clang
+        //    ++count;
+        //}
+        //std::cout << "diff: " << count << "\n";
+        //clang error: arithmetic on pointers to void
+        //std::cout << p5 - p << "\n"; // error: invalid use of ‘void’
     }
-    
+    {
+        int* p1 = &a[1];
+        int* p5 = &a[5];
+        std::cout << p5 - p1 << "\n";
+    }
+    {
+        unsigned int a = 10;
+        unsigned int b = 20;
+        auto c = a - b;
+        std::cout << "10 - 20 = " << c << "\n"; // 10 - 20 = 4294967286
+    }
     return 0;
 }
